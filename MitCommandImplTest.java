c3e254d85505e2a1469d5fe9cd61ab864317745a
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 15:05:08 +0900

    test: mit list 명령어 테스트 케이스의 출력방식 변경
    String 문자열을 그대로 출력하는 것에서 File 객체를 참조하여 출력에 맞게 변환하여 출력하도록 변경

--- MitCommandImplTest.java
+++ MitCommandImplTest.java
@@ -1,6 +1,7 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -12,8 +13,10 @@ class MitCommandImplTest {
         //given
         MitCommand mit = new MitCommandImpl();
         //when
-        List<String> list = mit.list("./Work/Masters");
+        List<File> list = mit.list("./Work/Masters");
         //then
-        list.forEach(System.out::println);
+        list.stream()
+            .map(file->String.format("%s %.2fKB", file.getName(), (double) file.length() / 1024))
+            .forEach(System.out::println);
     }
 }
