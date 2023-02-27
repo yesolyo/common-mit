19e61e4a54bb4e477d0d986e8aae5b9aed922f98
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 15:17:08 +0900

    feat: mit list 명령어 구현에서 Optinal 타입 적용
    입력으로 받은 디렉토리명이 디렉토리가 아닌 경우 경고 메시지를 출력하고 빈 Optional을 반환합니다.

--- MitCommandImpl.java
+++ MitCommandImpl.java
@@ -2,30 +2,31 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class MitCommandImpl implements MitCommand{
 
     @Override
-    public List<File> list(String directoryName) {
+    public Optional<List<File>> list(String directoryName) {
         File folder = new File(directoryName);
         List<File> result = new ArrayList<>();
 
         if(!folder.isDirectory()){
             System.out.printf("입력하신 디렉토리명은 디렉토리가 아닙니다. : %s%n", directoryName);
-            return null;
+            return Optional.empty();
         }
 
         Collections.addAll(result, folder.listFiles());
-        return result;
+        return Optional.ofNullable(result);
     }
 
     @Override
-    public List<String> hash(String directoryName) {
+    public Optional<List<String>> hash(String directoryName) {
         return null;
     }
 
     @Override
-    public List<String> zlib(String directoryName) {
+    public Optional<List<String>> zlib(String directoryName) {
         return null;
     }
 }
