0cb90e4e99fad029cb27522d8fdd3cffe6f81689
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 14:45:15 +0900

    feat: mit list 명령어 메시지의 반환타입을 File로 변경
    파일명과 파일의 크기를 출력하기 위해서 String 타입보다는 File 타입이 적합하다고 생각하였습니다.

--- MitCommand.java
+++ MitCommand.java
@@ -1,7 +1,8 @@
+import java.io.File;
 import java.util.List;
 
 public interface MitCommand {
-    List<String> list(String directoryName);
+    List<File> list(String directoryName);
     List<String> hash(String directoryName);
     List<String> zlib(String directoryName);
 }
