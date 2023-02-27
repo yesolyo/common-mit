904b6e21382533b5c4610dd798878f20d93a63a7
saehim <saeilee.vxv@gmail.com>
2023-02-27 16:31:22 +0900

    Feat (list) : list 커맨드시 파일 목록 탐색 후 크기와 파일명 출력 기능

--- MitCommand.java
+++ MitCommand.java
@@ -1,11 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Optional;
 
 public enum MitCommand {
     LIST("list"){
         @Override
-        void run(String directory) {
-            System.out.println("list가 실행됩니다.");
+        void run(String directory) throws IOException {
+            // 디렉토리에서 전체 파일 목록을 탐색하고, 파일 크기와 파일명을 출력한다
+            File dir = new File(directory);
+            char idx = 'a';
+
+            for (File file : dir.listFiles()) {
+                if (file.isHidden()) continue;  // 숨김 파일은 출력 생략
+                System.out.printf("%s. %s %dkb\n",idx++, file.getName(), file.length()/1024);
+            }
         }
     },
     HASH("hash"){
@@ -22,7 +32,6 @@ public enum MitCommand {
     };
 
     private String command;
-    private String reg = String.format("mit %s /[.]+", command);
 
     MitCommand(String command) {
         this.command = command;
@@ -32,5 +41,5 @@ public enum MitCommand {
         return Arrays.stream(values()).filter(e -> e.command.equals(str)).findFirst();
     }
 
-    abstract void run(String directory);
+    abstract void run(String directory) throws IOException;
 }
