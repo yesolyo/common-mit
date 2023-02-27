494312ed6044de2dca12d0ff908e18005e0c9e6c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 15:45:44 +0900

    test: mit list 명령어의 테스트 케이스 추가
    추가한 테스트 케이스는 적절하지 않은 디렉토리명 문자열이거나 문자열이 null인 경우의 테스트 케이스를 추가하였습니다. 적절하지 않은 문자열 값인 경우 빈 Optional 객체를 반환받습니다.

--- MitCommandImplTest.java
+++ MitCommandImplTest.java
@@ -1,6 +1,9 @@
-import org.junit.jupiter.api.Assertions;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.util.List;
@@ -19,6 +22,31 @@ class MitCommandImplTest {
             list.stream()
                     .map(file->String.format("%s %.2fKB", file.getName(), (double) file.length() / 1024))
                     .forEach(System.out::println);
-        }, ()->Assertions.fail("테스트에 실패하였습니다."));
+        }, ()-> Assertions.fail("테스트에 실패하였습니다."));
     }
+
+    @ParameterizedTest
+    @DisplayName("mit list 명령어 수행시 적절하지 않은 디렉토리명을 입력시 빈 리스트를 가져오는지 테스트")
+    @ValueSource(strings = {"", " ","./Work/Master", "#@#!", "1235"})
+    public void list_givenDirectoryName_whenMitList_thenEmptyFileList(String directoryName){
+        //given
+        MitCommand mit = new MitCommandImpl();
+        //when
+        Optional<List<File>> optionalList = mit.list(directoryName);
+        //then
+        Assertions.assertThat(optionalList.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("mit list 명령어 수행시 디렉토리명이 null인 경우 빈 리스트를 가져오는지 테스트")
+    public void list_givenDirectoryNameWithNull_whenMitList_thenEmptyFileList(){
+        //given
+        String directoryName = null;
+        MitCommand mit = new MitCommandImpl();
+        //when
+        Optional<List<File>> optionalList = mit.list(directoryName);
+        //then
+        Assertions.assertThat(optionalList.isEmpty()).isTrue();
+    }
+
 }
