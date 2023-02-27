94b3f76ab835a64eeda9fc747021845be0e505cd
saehim <saeilee.vxv@gmail.com>
2023-02-27 15:29:23 +0900

    Feat (command type) : 상수 구현

---
+++ CommandType.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum CommandType {
+    LIST("list"){
+        @Override
+        void run(String directory) {
+            System.out.println("list가 실행됩니다.");
+        }
+    },
+    HASH("hash"){
+        @Override
+        void run(String directory) {
+            System.out.println("hash가 실행됩니다.");
+        }
+    },
+    ZLIB("zlib"){
+        @Override
+        void run(String directory) {
+            System.out.println("zlib이 실행됩니다.");
+        }
+    };
+
+    private String command;
+    private String reg = String.format("mit %s /[.]+", command);
+
+    CommandType(String command) {
+        this.command = command;
+    }
+
+    public static Optional<CommandType> of (String str) {
+        return Arrays.stream(values()).filter(e -> e.command.equals(str)).findFirst();
+    }
+
+    abstract void run(String directory);
+}
