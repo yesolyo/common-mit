7453df684c78e355b82ca2761a9509feed86d842
JangHoonSeok <orirori512@naver.com>
2023-02-27 15:01:57 +0900

    ✨ feat: 입력받은 cmd 파싱하여 변수에 저장

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ this.stdio = this.readline.createInterface({
 });
 
 console.log("> 명령어를 입력해 주세요.");
+console.log("형식 : mit ");
 this.stdio.on("line", (cmd) => {
   if (cmd === "exit") {
     this.stdio.close();
+  }
+  const [commandName, subCommand, path] = cmd.split(" ");
+  if (commandName === "mit") {
+    switch (subCommand) {
+      case "list":
+        break;
+      case "hash":
+        break;
+      case "zlib":
+        break;
+    }
   }
 });
