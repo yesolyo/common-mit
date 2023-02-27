a8d8e799b1e6b0c4acff2eb3a12bf779d82b2853
Sully <2dcoder@naver.com>
2023-02-27 11:22:58 +0900

    feat: command 명령어 동일한지 확인

--- Command.java
+++ Command.java
@@ -1,6 +1,19 @@
 public class Command {
     private final String command;
+
     public Command(String input) {
         this.command = input;
+    }
+
+    public boolean isList() {
+        return command.equals("list");
+    }
+
+    public boolean isHash() {
+        return command.equals("hash");
+    }
+
+    public boolean isZlib() {
+        return command.equals("zlib");
     }
 }
