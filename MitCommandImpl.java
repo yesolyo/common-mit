6e7e9bd9f5289781e090e40fe0d9f64c0e764d5f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 14:47:22 +0900

    feat: mit list 명령어 구현
    입력으로 디렉터리명 문자열을 입력받고 해당 파일이 디렉터리가 아니라면 경고 메시지를 출력하고 null을 반환합니다.
    
    입력으로 받은 디렉터리명 문자열이 디렉터리라면 디렉터리의 각각의 파일명을 참조하고 File 객체로 만들어 결과 컬렉션 리스트에 저장하고 반환합니다.

--- MitCommandImpl.java
+++ MitCommandImpl.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MitCommandImpl implements MitCommand{
+
     @Override
-    public List<String> list(String directoryName) {
-        return null;
+    public List<File> list(String directoryName) {
+        File folder = new File(directoryName);
+        List<File> result = new ArrayList<>();
+
+        if(!folder.isDirectory()){
+            System.out.printf("입력하신 디렉토리명은 디렉토리가 아닙니다. : %s%n", directoryName);
+            return null;
+        }
+
+        for(String fileName : folder.list()){
+            result.add(new File(fileName));
+        }
+        return result;
     }
 
     @Override
